628cff9de042e82adf36548d5b24e6cd6650cf8b
jeremie Helme <jeremiehelme@gmail.com>
2024-07-15 21:46:07 +0200

    update packages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
   nitro: {
     preset: "vercel-edge",
   },
+
   css: ["~/assets/css/tailwind.css", "~/assets/css/main.scss"],
+
   build: {
     postcss: {
       postcssOptions: {
@@ -15,22 +17,28 @@ export default defineNuxtConfig({
     },
     transpile: ['leaflet', "@vue-leaflet/vue-leaflet"],
   },
+
   components: [
     { path: '~/components/button', prefix: 'Button' },
     '~/components'
   ],
+
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-graphql-client',
     'nuxt-svgo',
   ],
+
   svgo: {
     defaultImport: 'component'
   },
+
   runtimeConfig: {
     public: {
       GQL_HOST: process.env.GQL_HOST
     }
   },
-  devtools: { enabled: true }
+
+  devtools: { enabled: true },
+  compatibilityDate: '2024-07-15'
 })
